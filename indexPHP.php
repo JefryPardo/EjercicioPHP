03c4f344772d7c682cf6a224331b9913cdb98d9f
Jefry Pardo <jeffry.pardo01@unicatolica.edu.co>
2021-04-26 15:31:27 -0500

    save

---
+++ indexPHP.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Prueba</h1>
+    <table border=1>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                echo "<tr><td>398000</td>
+                <td>Jefry</td></tr>";
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
---
+++ proce.php
@@ -0,0 +1,54 @@
+<?php
+    // $nombre = "Jefry";
+    // $edad = 24;
+    // //echo "El señor".$nombre." tiene ".$edad." años";
+
+    // if($edad >= 18){
+    //     echo "El señor $nombre es mayor de edad.";
+    // }else{
+    //     echo "El señor $nombre es menor de edad.";
+    // }
+
+    // // for($i = 0; $i < $edad; $i++){
+    // //     echo "$i<br>";
+    // // }
+
+    // $miarreglo = [10,20,3,1];
+
+    // echo $miarreglo[0];
+
+    // $miarregloAsociativo = [
+    //     'nombre' => 'victor',
+    //     'edad' => 38
+    // ]
+
+    // echo $miarregloAsociativo['nombre']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // echo json_encode($_REQUEST);
+
+
+
+
+
+
+
+        $resp = [
+            
+        ]
+
+
+?>
